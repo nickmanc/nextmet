e7fc7ead2c95613912949cf21c247f5725faf584
ncooke <ncooke@heywood.co.uk>
2023-04-18 22:41:17 +0100

    initial commit, hardcoded to stretford/trafford bar

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# app/Dockerfile
+
+FROM python:3.9-slim
+
+WORKDIR /app
+
+ARG NEXTMET_BUILD_VERSION
+ARG GOOGLE_ANALYTICS_TAG
+
+ENV NEXTMET_BUILD_VERSION=${NEXTMET_BUILD_VERSION:-NOT_DEFINED}
+ENV GOOGLE_ANALYTICS_TAG=${GOOGLE_ANALYTICS_TAG:-"G-XXXXXXXX"}
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm  \
+    -rf /var/lib/apt/lists/*
+
+COPY . .
+
+RUN pip3 install -r requirements.txt
+
+RUN export STREAMLIT_LOCATION=$(pip3 show streamlit | grep Location | awk '{print $2}') && \
+    sed -i "s/<head>/<head>\n<!-- Google Analytics -->\n<script async src=\"https:\/\/www.googletagmanager.com\/gtag\/js?id=${GOOGLE_ANALYTICS_TAG}\"><\/script>\n<script>\n\twindow.dataLayer = window.dataLayer || [];\n\tfunction gtag() {dataLayer.push(arguments);}\n\tgtag('js', new Date());\n\tgtag('config', '${GOOGLE_ANALYTICS_TAG}');\n<\/script>\n/g" $STREAMLIT_LOCATION/streamlit/static/index.html
+
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "nextmet.py", "--server.port=8501", "--server.address=0.0.0.0"]
---
+++ nextmet.py
@@ -0,0 +1,120 @@
+import os
+
+import streamlit as st
+from datetime import datetime
+from streamlit_autorefresh import st_autorefresh
+
+from metrolink_lines import get_metrolink_line_status
+from trams import get_tram_departures
+
+NO_TRAM_SCHEDULED_MESSAGE = "No trams currently scheduled to depart."
+
+st.set_page_config(layout="wide")
+st.markdown(
+    """
+        <style>
+            .appview-container .main .block-container {{
+                padding-top: {padding_top}rem;
+                padding-bottom: {padding_bottom}rem;
+                }}
+
+        </style>""".format(
+        padding_top=0, padding_bottom=1
+    ),
+    unsafe_allow_html=True,
+)
+
+# st_autorefresh(interval=60 * 1000)
+st_autorefresh(interval=15 * 1000)
+
+#if before 11am
+if datetime.now().hour < 11:
+    from_container = st.container()
+    # Display tram information in a table
+    from_container.subheader(f'[{os.environ["child2_morning_name"]}]({os.environ["child2_morning_url"]})')
+    tramDepartureInfo = get_tram_departures({os.environ["child2_morning_ids"]})
+    trams = tramDepartureInfo[0]
+    if len(trams) > 0:
+        for tram in trams:
+            if tram['expected'] == 0:
+                tramExpectedText = "NOW"
+            else:
+                tramExpectedText = f"in **{tram['expected']}** minutes."
+            from_container.markdown(f"**{tram['destination']}**  ({tram['carriages']}) **{tramExpectedText}**")
+    else:
+        from_container.markdown(NO_TRAM_SCHEDULED_MESSAGE)
+
+to_container = st.container()
+to_container.subheader(f'[{os.environ["child2_afternoon_name"]}]({os.environ["child2_afternoon_url"]})')
+tramDepartureInfo = get_tram_departures({os.environ["child2_afternoon_ids"]})
+trams = tramDepartureInfo[0]
+if len(trams) > 0:
+    for tram in trams:
+        if tram['expected'] == 0:
+            tramExpectedText = "NOW"
+        else:
+            tramExpectedText = f"in **{tram['expected']}** minutes."
+        to_container.markdown(f"**{tram['destination']}**  ({tram['carriages']}) **{tramExpectedText}**")
+else:
+    to_container.markdown(NO_TRAM_SCHEDULED_MESSAGE)
+
+
+#if after 11am
+if datetime.now().hour >= 11:
+    from_container = st.container()
+    # Display tram information in a table
+    from_container.subheader(f'[{os.environ["child2_morning_name"]}]({os.environ["child2_morning_url"]})')
+    tramDepartureInfo = get_tram_departures({os.environ["child2_morning_ids"]})
+    trams = tramDepartureInfo[0]
+    if len(trams) > 0:
+        for tram in trams:
+            if tram['expected'] == 0:
+                tramExpectedText = "NOW"
+            else:
+                tramExpectedText = f"in **{tram['expected']}** minutes."
+            from_container.markdown(f"**{tram['destination']}**  ({tram['carriages']}) **{tramExpectedText}**")
+    else:
+        from_container.markdown(NO_TRAM_SCHEDULED_MESSAGE)
+
+
+st.subheader("[Metrolink Status](https://tfgm.com/live-updates)")
+metrolinkLineStatuses = get_metrolink_line_status()
+if len(metrolinkLineStatuses) > 0:
+    for metrolinkLineStatus in metrolinkLineStatuses:
+        if metrolinkLineStatus.get('severity') == 'danger':
+            statusColour = "red"
+        elif metrolinkLineStatus.get('severity') == 'warning':
+            statusColour = "orange"
+        else:
+            statusColour = "green"
+
+        st.markdown(f"**{metrolinkLineStatus['name']}** - :{statusColour}[{metrolinkLineStatus['status']}]")
+else:
+    st.write("No line status available")
+
+footer = f"""<style>
+  .footer {{
+    display: flex; /* display the elements in a row */
+    justify-content: space-between; /* space the elements evenly */
+    align-items: center; /* center the items vertically */
+    padding: 10px 0; /* add some padding to the top and bottom */
+    width: 100%; /* set the width to 100% */
+    color: grey;
+    justify-content: space-around;
+  }}
+  
+  .footer td {{
+    border: none; /* remove the borders from the table cells */
+  }}
+</style>
+<div class="footer">
+  <table>
+    <tr>
+      <td>{datetime.now().strftime('%H:%M:%S')}</td>
+      <td>Contains <a href="https://www.tfgm.com">TfGM</a> data</td>      
+      <td>{os.getenv("NEXTMET_BUILD_VERSION", "local")}</td>
+    </tr>
+  </table>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
